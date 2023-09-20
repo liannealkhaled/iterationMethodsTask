579682ab4b1eaa7c216653b82bd8df9975f985a5
lianne <liannealq@hotmail.com>
2023-09-21 00:28:32 +0300

    20/9

--- index.js
+++ index.js
@@ -9,43 +9,61 @@ Write code to perform the following tasks using the filter method:
 Q1) Create a new array containing numbers that are greater than or equal to 25.
 Q2) Create a new array containing numbers that are divisible by 5.
 ******************************************************************/
-let newarray =[ ]
-newarray = numbers.filter((x)=> {return x>= 25})
-console.log(newarray)
 
-let divisibleByfive =[ ]
-divisibleByfive = numbers.filter((x)=> {return x%5 == 0 })
-console.log(divisibleByfive)
+let Numbers25 = [ ]
+
+Numbers25 = numbers.filter( (number)=>{return number >= 25} )
+console.log(Numbers25)
 
 
-/*****************************************************************
-Part 2: Mapping
 
-Write code to perform the following tasks using the map method:
+let Numbersby5 = []
+Numbersby5 = numbers.filter( (number)=>{return number%5==0} )
+console.log(Numbersby5)
 
-Q3) Create a new array that contains each number squared.
-Q4) Create a new array that contains each number multiplied by 2.
-******************************************************************/
-let squaredarray = []
-squaredarray = numbers.map((x)=> x*x )
-console.log(squaredarray)
 
-let multipliedarray = [ ]
-multipliedarray = numbers.map ((x)=> x*2)
-console.log(multipliedarray)
-/*****************************************************************
-Part 3: Combining Filtering and Mapping
 
-Combine the filter and map methods to perform the following tasks:
 
-Q5) Filter the numbers that are greater than or equal to 20 and then square each of them.
-Q6) Filter the numbers that are divisible by 5 and then multiply each of them by 3.
-******************************************************************/
-let arrayq5 = []
-arrayq5 = numbers.filter((x)=> x>= 20).map((x)=> x*x )
+// /*****************************************************************
+// Part 2: Mapping
 
-console.log(arrayq5)
+// Write code to perform the following tasks using the map method:
 
-let arrayq6 =[]
-arrayq6 = numbers.filter((x)=> x%5==0).map((x)=> x*3)
-console.log(arrayq6)
+// Q3) Create a new array that contains each number squared.
+// Q4) Create a new array that contains each number multiplied by 2.
+// ******************************************************************/
+// 
+let sqaured = []
+sqaured = numbers.map ( (number)=>{return number*number} )
+console.log(sqaured)
+
+
+let multiplied = [ ]
+multiplied = numbers.map( (number)=>number*2 ) 
+console.log(multiplied)
+
+
+
+// /*****************************************************************
+// Part 3: Combining Filtering and Mapping
+
+// Combine the filter and map methods to perform the following tasks:
+
+// Q5) Filter the numbers that are greater than or equal to 20 and then square each of them.
+// Q6) Filter the numbers that are divisible by 5 and then multiply each of them by 3.
+// ******************************************************************/
+
+let Q5 = []
+Q5 = numbers.filter((num)=>{return num>=20}).map((num)=>{return num*num} ) 
+
+console.log(Q5)
+
+
+
+
+
+let Q6 =[]
+Q6 = numbers.filter((x)=>x%5==0  ).map((x)=>x*3 ) 
+console.log(Q6)
+
+
